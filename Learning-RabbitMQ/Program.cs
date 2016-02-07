4102c55fa0b2c40adddd50842c29de80c3437561
Sapan Desai <imsapan4u@yahoo.com>
2016-02-07 20:51:39 +0000

    Initial draft.

---
+++ Learning-RabbitMQ/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Learning_RabbitMQ
+{
+    public class Send
+    {
+        static void Main(string[] args)
+        {
+            SendMessage();
+        }
+
+        private static void SendMessage()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("Hello Queue", false, false, false, null);
+
+                    string message = "Welcome Bindi!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "Hello Queue",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
